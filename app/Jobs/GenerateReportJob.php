cba7bafc956c07c6203795d0be90230f8e1b2e5a
Amit <amit@culture-red.com>
2025-09-26 17:36:27 +0530

    array collection for generate report service params

--- app/Jobs/GenerateReportJob.php
+++ app/Jobs/GenerateReportJob.php
@@ -20,7 +20,7 @@ class GenerateReportJob implements JobInterface
         if(isset($data['assessment_id']) && !empty($data['assessment_id'])){
             $assessment = AssessmentModel::with('user', 'payment')->find($data['assessment_id']);
             if(!empty($assessment)){
-                $this->assessmentReportService->generateReport(collect([$assessment]), 'single');
+                $this->assessmentReportService->generateReport([$assessment], 'single');
             }
         }
     }
